8469a5d3eb30ebfaa7f85b063c9a1c71182d9437
Gerard Eklu <gerard_eklu@homedepot.com>
2021-07-05 08:53:43 -0400

    Building this safety team profile in angular

---
+++ src/app/mock-user.ts
@@ -0,0 +1,25 @@
+import {User} from './User';
+
+export const USERS: User[] = [
+  {
+    id: 1,
+    firstName: 'Kubby',
+    lastName: 'Issacs',
+    role: 'Software Engineer Manager',
+    image: 'https://e7.pngegg.com/pngimages/122/453/png-clipart-computer-icons-user-profile-avatar-female-profile-heroes-head.png'
+  },
+  {
+    id: 2,
+    firstName: 'Nagini',
+    lastName: 'Issacs',
+    role: 'Software Engineer Manager',
+    image: 'https://e7.pngegg.com/pngimages/122/453/png-clipart-computer-icons-user-profile-avatar-female-profile-heroes-head.png'
+  },
+  {
+    id: 3,
+    firstName: 'Michelle',
+    lastName: 'Issacs',
+    role: 'Software Engineer Manager',
+    image: 'https://e7.pngegg.com/pngimages/122/453/png-clipart-computer-icons-user-profile-avatar-female-profile-heroes-head.png'
+  }
+]
